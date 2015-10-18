3cd0f1fe9f154338229655e0e499aaa554acd235
Lewis S Notestine <lewis.notestine@gmail.com>
2015-10-17 17:51:08 -0700

    started using dimple instead of plain ol d3.
    Not quite working yet though.

--- protoApp/app/scripts/whoIsGivingChart.js
+++ protoApp/app/scripts/whoIsGivingChart.js
@@ -1,48 +1,22 @@
     
 var renderWhoIsGivingChart = function() {
+    var svg = dimple.newSvg("#whoIsGivingChart", 590, 400);
+    d3.csv("who-is-giving.csv", function (data) {
 
-    var data = d3.csv.parse(d3.select('#csv').text());
-    var valueLabelWidth = 40; // space reserved for value labels (right)    
-    var barHeight = 20; // height of one bar
-    var barLabelWidth = 100; // space reserved for bar labels
-    var barLabelPadding = 5; // padding between bar and bar labels (left)
-    var gridLabelHeight = 18; // space reserved for gridline labels
-    var gridChartOffset = 3; // space between start of grid and first bar
-    var maxBarWidth = 420; // width of the bar with the max value
-     
-    // accessor functions 
-    var barLabel = function(d) { return d['DonorType']; };
-    var barValue = function(d) { return parseFloat(d['Amount (mill)']); };
-     
-    // scales
-    var yScale = d3.scale.ordinal().domain(d3.range(0, data.length)).rangeBands([0, data.length * barHeight]);
-    var y = function(d, i) { return yScale(i); };
-    var yText = function(d, i) { return y(d, i) + yScale.rangeBand() / 2; };
-    var x = d3.scale.linear().domain([0, d3.max(data, barValue)]).range([0, maxBarWidth]);
+      //HACK. for some reason jquery.each() wasn't working that well for this... 
+      var dataKeys = Object.keys(data[0]);
+      for (var i = 0; i < data.length; i++) { 
+        var key = dataKeys[i];        
+        var intValue = parseInt(data[0][key]);  
+        data[0][key] = intValue;
+      } 
+
+      var myChart = new dimple.chart(svg, data);
+      myChart.setBounds(60, 30, 510, 305)
+      var x = myChart.addCategoryAxis("x", "Source");
     
-    // svg container element
-    var chart = d3.select('#whoIsGivingChart').append("svg")
-      .attr('width', maxBarWidth + barLabelWidth + valueLabelWidth)
-      .attr('height', gridLabelHeight + gridChartOffset + data.length * barHeight);
-    
-    // bar labels
-    var labelsContainer = chart.append('g')
-      .attr('transform', 'translate(' + (barLabelWidth - barLabelPadding) + ',' + (gridLabelHeight + gridChartOffset) + ')'); 
-    labelsContainer.selectAll('text').data(data).enter().append('text')
-      .attr('y', yText)
-      .attr('stroke', 'none')
-      .attr('fill', 'black')
-      .attr("dy", ".35em") // vertical-align: middle
-      .attr('text-anchor', 'end')
-      .text(barLabel);
-    
-    // bars
-    var barsContainer = chart.append('g')
-      .attr('transform', 'translate(' + barLabelWidth + ',' + (gridLabelHeight + gridChartOffset) + ')'); 
-    barsContainer.selectAll("rect").data(data).enter().append("rect")
-      .attr('y', y)
-      .attr('height', yScale.rangeBand())
-      .attr('width', function(d) { return x(barValue(d)); })
-      .attr('stroke', 'white')
-      .attr('fill', 'steelblue');
+      myChart.addMeasureAxis("y", "Amount (millions)");
+      myChart.addSeries(null, dimple.plot.bar);
+      myChart.draw();
+    });
 }
